4ac89415c7caff553cf537fc8012d11eac061034
weiweili1207 <lwei6793@gmail.com>
2024-09-16 23:14:12 -0700

    dynamic thread pool service create

--- dynamic-thread-pool-spring-boot-starter/src/main/java/com/wei/middleware/dynamic/thread/pool/sdk/domain/DynamicThreadPoolService.java
+++ dynamic-thread-pool-spring-boot-starter/src/main/java/com/wei/middleware/dynamic/thread/pool/sdk/domain/DynamicThreadPoolService.java
@@ -1,9 +1,32 @@
 package com.wei.middleware.dynamic.thread.pool.sdk.domain;
+
+import com.wei.middleware.dynamic.thread.pool.sdk.domain.model.entity.ThreadPoolConfigEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
 /*
  *@description dynamic thread pool service-get data
  *@author wei li
- *@create 9/15/2024
+ *@create 9/16/2024
  * */
-public interface DynamicThreadPoolService {
+public class DynamicThreadPoolService implements IDynamicThreadPoolService{
 
+    private final Logger logger = LoggerFactory.getLogger(DynamicThreadPoolService.class);
+
+    @Override
+    public List<ThreadPoolConfigEntity> queryThreadPoolList() {
+        return List.of();
+    }
+
+    @Override
+    public ThreadPoolConfigEntity queryThreadPoolListByName(String threadPoolName) {
+        return null;
+    }
+
+    @Override
+    public void updateThreadPoolConfig(ThreadPoolConfigEntity threadPoolConfigEntity) {
+
+    }
 }
