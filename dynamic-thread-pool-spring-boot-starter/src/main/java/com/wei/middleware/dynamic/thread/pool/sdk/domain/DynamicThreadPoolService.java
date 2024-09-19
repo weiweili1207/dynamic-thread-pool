5f26563f2a245ada1e9a2d5ed19e7df06f18e5ff
weiweili1207 <lwei6793@gmail.com>
2024-09-18 22:56:36 -0700

    Collect thread pool config finish

--- dynamic-thread-pool-spring-boot-starter/src/main/java/com/wei/middleware/dynamic/thread/pool/sdk/domain/DynamicThreadPoolService.java
+++ dynamic-thread-pool-spring-boot-starter/src/main/java/com/wei/middleware/dynamic/thread/pool/sdk/domain/DynamicThreadPoolService.java
@@ -1,5 +1,6 @@
 package com.wei.middleware.dynamic.thread.pool.sdk.domain;
 
+import com.alibaba.fastjson.JSON;
 import com.wei.middleware.dynamic.thread.pool.sdk.domain.model.entity.ThreadPoolConfigEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -50,11 +51,36 @@ public class DynamicThreadPoolService implements IDynamicThreadPoolService{
 
     @Override
     public ThreadPoolConfigEntity queryThreadPoolListByName(String threadPoolName) {
-        return null;
+        ThreadPoolExecutor threadPoolExecutor = threadPoolExecutorMap.get(threadPoolName);
+        if (threadPoolExecutor == null) {
+            return new ThreadPoolConfigEntity(applicationName, threadPoolName);
+        }
+        //thread pool config data
+        ThreadPoolConfigEntity threadPoolConfigVO = new ThreadPoolConfigEntity(applicationName, threadPoolName);
+        threadPoolConfigVO.setCorePoolSize(threadPoolExecutor.getCorePoolSize());
+        threadPoolConfigVO.setMaximumPoolSize(threadPoolExecutor.getMaximumPoolSize());
+        threadPoolConfigVO.setActiveCount(threadPoolExecutor.getActiveCount());
+        threadPoolConfigVO.setPoolSize(threadPoolExecutor.getPoolSize());
+        threadPoolConfigVO.setQueueType(threadPoolExecutor.getQueue().getClass().getTypeName());
+        threadPoolConfigVO.setQueueSize(threadPoolExecutor.getQueue().size());
+        threadPoolConfigVO.setRemainingCapacity(threadPoolExecutor.getQueue().remainingCapacity());
+        if (logger.isDebugEnabled()) {
+            logger.info("dynamic thread pool, configuration query application name: {} thread pool name: {} pooling configuration: {}", applicationName, threadPoolName, JSON.toJSONString(threadPoolConfigVO));
+        }
+        return threadPoolConfigVO;
     }
 
     @Override
     public void updateThreadPoolConfig(ThreadPoolConfigEntity threadPoolConfigEntity) {
+        if (threadPoolExecutorMap == null || !applicationName.equals(threadPoolConfigEntity.getAppName())) {
+            return;
+        }
+        ThreadPoolExecutor threadPoolExecutor = threadPoolExecutorMap.get(threadPoolConfigEntity.getThreadPoolName());
+        if (threadPoolExecutor == null) {
+            return;
+        }
 
+        threadPoolExecutor.setCorePoolSize(threadPoolConfigEntity.getCorePoolSize());
+        threadPoolExecutor.setMaximumPoolSize(threadPoolConfigEntity.getMaximumPoolSize());
     }
 }
