c05a9d544487604ea71df457606142494a6822d5
weiweili1207 <lwei6793@gmail.com>
2024-10-04 23:09:37 -0700

    Admin project construction

---
+++ dynamic-thread-pool-admin/src/main/java/com/wei/middleware/dynamic/thread/pool/trigger/DynamicThreadPoolController.java
@@ -0,0 +1,14 @@
+package com.wei.middleware.dynamic.thread.pool.trigger;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController()
+@CrossOrigin("*")
+@RequestMapping("api/v1/dynamic/thread/pool")
+public class DynamicThreadPoolController {
+
+}
